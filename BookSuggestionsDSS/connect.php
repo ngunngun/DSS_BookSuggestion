164f8a4130aa27fde736932fd9ea630fae9527ee
ngunngun <ngunngun>
2017-03-24 18:21:15 +0700

    Fix folder

--- BookSuggestionsDSS/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $conn = mysqli_connect("localhost","root","dsslamom01", "DSS");
-
-    if (mysqli_connect_errno($mysqli)) {
-        echo "Failed to connect to MySQL: " . mysqli_connect_error();
-    }
-    mysqli_query("SET NAMES UTF8");
-
-?>
--- README.md
+++
@@ -1,2 +0,0 @@
-# DSS_BookSuggestion
-Decision Support System's term project
